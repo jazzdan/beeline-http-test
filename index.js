22cd8a0d9573ead7b7319596696cf632e075c7b1
Dan Miller <dan@replay.io>
2021-09-01 15:40:16 -0400

    this doesn't work either 😕

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require("honeycomb-beeline")({
+const beeline = require("honeycomb-beeline")({
   writeKey: "YOUR_API_KEY",
   dataset: "my-node-distributed-app",
   serviceName: "my-node-service",
   transmission: "console",
+  disableInstrumentation: false,
 });
 
 const http = require("http");
@@ -10,11 +11,8 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const beeline = require("honeycomb-beeline")();
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  beeline.addContext({ statusCode: res.statusCode });
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World");
 });
